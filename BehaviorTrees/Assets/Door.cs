960fc2a017699682f4032bbffeacc39772cb3e43
Derek Scandrett <scandred@my.erau.edu>
2024-03-30 20:46:24 -0700

    More animations

--- BehaviorTrees/Assets/Door.cs
+++ BehaviorTrees/Assets/Door.cs
@@ -19,6 +19,11 @@ public class Door : MonoBehaviour
 	private GameObject BrokenDoor;
 	[SerializeField]
 	private GameObject LockIndicator;
+	[SerializeField]
+	private GameObject AnimatedDoor;
+
+	[SerializeField]
+	private Animator DoorAnimator;
 
 
 	void Start()
@@ -37,54 +42,73 @@ public class Door : MonoBehaviour
 
 	public void Reset()
 	{
-		if (DoorBroken) {
-			DoorOpen = true;
-			DoorLocked = false;
-		}
+		DoorBroken = false;
 		if (DoorLocked) {
 			DoorOpen = false;
 		}
 
-		BrokenDoor.SetActive(DoorBroken);
+		//BrokenDoor.SetActive(DoorBroken);
 		
-		ClosedDoor.SetActive(!DoorOpen);
-		OpenedDoor.SetActive(DoorOpen);
+		//ClosedDoor.SetActive(!DoorOpen);
+		//OpenedDoor.SetActive(DoorOpen);
 
 		LockIndicator.SetActive(DoorLocked);
+
+
+		OpenedDoor.SetActive(false);
+		ClosedDoor.SetActive(false);
+		BrokenDoor.SetActive(false);
+		AnimatedDoor.SetActive(true);
+
+		DoorAnimator.SetBool("DoorOpen", DoorOpen);
+		DoorAnimator.SetBool("DoorLocked", DoorLocked);
+		DoorAnimator.SetBool("DoorBroken", DoorBroken);
 	}
 
 	public IEnumerator OpenDoor()
 	{
 		if (DoorLocked) yield break;
-		ClosedDoor.SetActive(false);
-		OpenedDoor.SetActive(true);
+		//ClosedDoor.SetActive(false);
+		//OpenedDoor.SetActive(true);
 		DoorOpen = true;
+		DoorAnimator.SetBool("DoorOpen", DoorOpen);
 		//DoorOpenUi.isOn = true;
+
 		Debug.Log("Door: OpenDoor");
+		yield return new WaitForSeconds(0.5f);
 		yield break;
 	}
 
 	public IEnumerator CloseDoor()
 	{
-		OpenedDoor.SetActive(false);
-		ClosedDoor.SetActive(true);
+		//OpenedDoor.SetActive(false);
+		//ClosedDoor.SetActive(true);
 		DoorOpen = false;
+		DoorAnimator.SetBool("DoorOpen", DoorOpen);
 		//DoorOpenUi.isOn = true;
+
 		Debug.Log("Door: CloseDoor");
+		yield return new WaitForSeconds(0.5f);
 		yield break;
 	}
 
 	public IEnumerator BargeDoor()
 	{
 		DoorLocked = false;
+		DoorAnimator.SetBool("DoorLocked", DoorLocked);
 		//DoorLockedUi.isOn = false;
 		LockIndicator.SetActive(false);
-		ClosedDoor.SetActive(false);
-		OpenedDoor.SetActive(false);
-		BrokenDoor.SetActive(true);
-		DoorOpen = true;
+		//ClosedDoor.SetActive(false);
+		//OpenedDoor.SetActive(false);
+		//BrokenDoor.SetActive(true);
+		DoorBroken = true;
+		DoorAnimator.SetBool("DoorBroken", DoorBroken);
 		//DoorOpenUi.isOn = true;
+
 		Debug.Log("Door: BargeDoor");
+		yield return new WaitForSeconds(0.5f);
+		DoorOpen = true;
+		DoorAnimator.SetBool("DoorOpen", DoorOpen);
 		yield break;
 	}
 
@@ -93,8 +117,11 @@ public class Door : MonoBehaviour
 	{
 		LockIndicator.SetActive(false);
 		DoorLocked = false;
+		DoorAnimator.SetBool("DoorLocked", DoorLocked);
 		//DoorLockedUi.isOn = false;
+
 		Debug.Log("Door: UnlockDoor");
+		yield return new WaitForSeconds(0.2f);
 		yield break;
 	}
 
@@ -103,8 +130,11 @@ public class Door : MonoBehaviour
 		if (DoorOpen) yield break;
 		LockIndicator.SetActive(true);
 		DoorLocked = true;
+		DoorAnimator.SetBool("DoorLocked", DoorLocked);
 		//DoorLockedUi.isOn = true;
+
 		Debug.Log("Door: LockDoor");
+		yield return new WaitForSeconds(0.2f);
 		yield break;
 	}
 }
